419648d1fae9e0b698d30517e0e6097169a6cacb
Ludmila <zozulyalud@yandex.ru>
2021-09-23 10:59:34 +0300

    cсоздание try except от 23.09

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,5 +7,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
        #login_link = browser.find_element_by_css_selector("#login_link") меняем эту строку на 
        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-       #time.sleep(10)
        login_link.click()
+       
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"),"Login link is not presented"
