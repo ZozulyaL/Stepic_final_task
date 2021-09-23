51c55fc9bb3165a63ac3efe0fab0483b01d2ac83
Ludmila <zozulyalud@yandex.ru>
2021-09-23 16:55:14 +0300

    cсоздание login and test от 23.09

---
+++ pages/login_page.py
@@ -0,0 +1,41 @@
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    #def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        #assert True
+
+    #def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        #assert True
+
+    #def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        #assert True
+        
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "'login' not in current url"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password, browser):
+        input1 = browser.find_element(By.CSS_SELECTOR, "#id_registration-email")
+        input1.send_keys(email)
+        input2 = browser.find_element(By.CSS_SELECTOR, "#id_registration-password1")
+        input2.send_keys(password)
+        input3 = browser.find_element(By.CSS_SELECTOR, "#id_registration-password2")
+        input3.send_keys(password)
+        button_register = browser.find_element(By.CSS_SELECTOR, "#register_form .btn-primary")
+        button_register.click()
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,14 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from pages.locators import MainPageLocators
 import time
 
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
        #login_link = browser.find_element_by_css_selector("#login_link") меняем эту строку на 
-       login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+       login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
        login_link.click()
        
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"),"Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK),"Login link is not presented"
