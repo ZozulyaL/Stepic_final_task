904a24717f93fb3d14c54400f8923a288ca154ce
Ludmila <zozulyalud@yandex.ru>
2021-10-06 14:16:29 +0300

    deleted comment

--- test_product_page.py
+++ test_product_page.py
@@ -9,7 +9,6 @@ import pytest
 @pytest.mark.parametrize('promo', [ "?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4", "?promo=offer5", "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail) , "?promo=offer8", "?promo=offer9",  ])
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, promo):
-    #link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/{promo}"
     link = f" http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promo}"
     page = ProductPage(browser, link)
     page.open()
